a45a6a46e2fb18a368088b2f5bd765dc54ed8266
Vaibhav <vaibhavdubey1999@gmail.com>
2023-04-07 22:02:41 +0530

    common list operations

---
+++ lists/listOperations.py
@@ -0,0 +1,23 @@
+# list operations 
+
+##concatenation + operator
+
+a= [1,2,3,4]
+b=[1,2,3,5]
+print(a+b)  #concatenates the two lists 
+## * operation
+c=[0]
+print(c*4)
+
+## len() function counts the number of integers in the list 
+
+d=[1,2,3,4]
+print(len(d))
+
+## max / min function to return max in the list 
+
+e=[1,2,3,5]
+print(max(e),min(e))
+
+## sum function to find some of elements in the list 
+print(sum(e))
