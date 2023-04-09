a36d553b882d27dd0b4c1da23f3377dbe495b1b5
Vaibhav <vaibhavdubey1999@gmail.com>
2023-04-09 19:56:56 +0530

    twoSum n2 solution

--- lists/twoSum.py
+++ lists/twoSum.py
@@ -1,21 +1,12 @@
 #find a program  to find all pairs of integers whose sum is equal to a given number 
 
 def twoSum(arr1,t):
-    indexArr=[]
-    mainIndex=[]
-    arr2=arr1
     for i in range(len(arr1)):
-        print(arr1[i])
-        searchNum= (t - arr1[i])
-        print("searchNum",searchNum)
-        arr2.remove(arr2[i])
-        print(arr2)
-        if(searchNum in arr2):
-            indexArr.append(i)
-            indexArr.append(arr2.index(searchNum) + 1)
-            mainIndex.append(indexArr)
-        else:continue
+        for j in range(i+1,len(arr1)-1):
+            if(arr1[i] + arr1[j]==t):
+                print(i,j)
+            else:continue
 
-    return mainIndex
+    
 
 print(twoSum([2,6,3,8,11],9))
