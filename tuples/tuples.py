750651daea580105a732762da50405616628ceb0
Vaibhav <vaibhavdubey1999@gmail.com>
2023-04-11 19:56:10 +0530

    tuples basics

---
+++ tuples/tuples.py
@@ -0,0 +1,32 @@
+myTuple = (1,2,3,4)
+myTuple1 =  (1,2,6,7,9)
+
+# '+'operation
+
+print(myTuple + myTuple1)
+
+# '*' operation
+
+print(myTuple * 4)
+
+# in operation
+
+print(4 in myTuple)
+
+# count operation
+
+print(myTuple.count(2))
+
+#index opertion 
+
+print(myTuple.index(3))
+
+#len operation 
+
+print(len(myTuple))
+
+## max min can be used in similar way as above 
+
+## tuple method can be used to convert list to tuple 
+
+print(tuple([1,2,4,5]))
