a8e6d22ed4f8ef4755b3690c17a53d75a8360e38
Vaibhav <vaibhavdubey1999@gmail.com>
2023-04-28 20:12:40 +0530

    isoMorphic Strings

---
+++ LeetCode_Problems/isomorphicString.py
@@ -0,0 +1,25 @@
+'''Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+'''
+
+def isIsomorphic(s,t):
+    if len(s) != len(t):
+        return False
+    else:
+        map1={}
+        map2={}
+        for i in range(len(s)):
+            ch1,ch2 = s[i],t[i]
+            if ch1 not in map1:
+                map1[ch1] = ch2
+            if ch2 not in map2:
+                map2[ch2] = ch1
+            
+            if map1[ch1] != ch2 or map2[ch2] != ch1:
+                return False
+        return True
+    
+print(isIsomorphic("foo","bar"))
